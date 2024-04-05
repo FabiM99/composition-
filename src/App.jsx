2731ce344f354d7ba67638af5a06bf14ec443631
fabian mardale <fabianmardale99@yahoo.it>
2024-04-05 09:04:44 +0200

    added exercise

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import { Container } from "./Container"
+import { Welcome } from "./welcome"
+
+
+
+export function App(){
+  return (
+    <Container title={<Welcome/>}>
+    
+      <h3>The exercise has be done by Lucy Linez and Fabian Mardale</h3>
+    
+    
+    </Container>
+  )
+}
